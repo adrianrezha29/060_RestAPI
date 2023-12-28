f804c84573705f6479c1855f10161a7a742d4779
adrianrezha29 <adrianrezha29@gmail.com>
2023-12-28 22:34:29 +0700

    add fun KontakLayout on dataclass HomeScreen on screen package

--- app/src/main/java/com/example/act13/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/act13/ui/home/screen/HomeScreen.kt
@@ -1,11 +1,15 @@
 package com.example.act13.ui.home.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
@@ -60,6 +64,30 @@ fun KontakCard(
             Text(
                 text = kontak.alamat,
                 style = MaterialTheme.typography.titleMedium,
+            )
+        }
+    }
+}
+@Composable
+fun KontakLayout(
+    kontak : List<Kontak>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Kontak) -> Unit,
+    onDeleteClick: (Kontak) -> Unit = {}
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)){
+        items(kontak){kontak ->
+            KontakCard(
+                kontak = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) },
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
             )
         }
     }
